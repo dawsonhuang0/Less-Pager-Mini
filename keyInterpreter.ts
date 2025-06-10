765b6efb679b141255cd086eb1f32482e0b2ff54
Dawson Huang <dawsonhuang0@gmail.com>
2025-06-10 13:47:05 +1000

    add: window backward and forward cases

--- keyInterpreter.ts
+++ keyInterpreter.ts
@@ -1,4 +1,8 @@
-const UP = '\u001b[A', DOWN = '\u001b[B';
+const UP = '\u001b[A', DOWN = '\u001b[B', SPACE = '\x20',
+  CTRL_E = '\x05', CTRL_N = '\x0E', CTRL_Y = '\x19',
+  CTRL_K = '\x0B', CTRL_P = '\x10', CTRL_F = '\x06',
+  CTRL_V = '\x16', CTRL_B = '\x02', CTRL_D = '\x04',
+  CTRL_U = '\x15', ESC_V = '\x1Bv';
 
 export enum Actions {
   // basic
@@ -25,11 +29,25 @@ export enum Actions {
 
 export function action(key: string): Actions | undefined {
   switch (key) {
+    case CTRL_Y:
+    case CTRL_K:
+    case CTRL_P:
     case UP:
       return Actions.LINE_BACKWARD;
 
+    case CTRL_E:
+    case CTRL_N:
     case DOWN:
       return Actions.LINE_FORWARD;
+
+    case CTRL_B:
+    case ESC_V:
+      return Actions.WINDOW_BACKWARD;
+
+    case CTRL_F:
+    case CTRL_V:
+    case SPACE:
+      return Actions.WINDOW_FORWARD;
   }
 
   switch (key.toLowerCase()) {
@@ -42,9 +60,22 @@ export function action(key: string): Actions | undefined {
     case 'v':
       return Actions.VERSION;
 
-    
+    case 'y':
+    case 'k':
+      return Actions.LINE_BACKWARD;
 
-    default:
-      return undefined;
+    case 'e':
+    case 'j':
+      return Actions.LINE_FORWARD;
+
+    case 'b':
+    case 'w':
+      return Actions.WINDOW_BACKWARD;
+
+    case 'f':
+    case 'z':
+      return Actions.WINDOW_FORWARD;
   }
+
+  return undefined;
 }
