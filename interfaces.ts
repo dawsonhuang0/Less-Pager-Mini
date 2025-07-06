70ea58e9840a8cab8ed111cf1e04f65bdd0fa44e
Dawson Huang <dawsonhuang0@gmail.com>
2025-07-06 16:42:07 +1000

    add: row, col to track page position and chopLongLines

--- interfaces.ts
+++ interfaces.ts
@@ -1,9 +1,14 @@
 export interface Config {
+  row: number;
+  col: number;
+
   window: number;
   halfWindow: number;
 
   screenWidth: number;
   halfScreenWidth: number;
+
+  chopLongLines: boolean;
 
   indentation: number;
 }
--- pagerConfig.ts
+++ pagerConfig.ts
@@ -1,9 +1,12 @@
 import { Config } from "./interfaces";
 
 export const config: Config = {
+  row: 0,
+  col: 0,
   window: process.stdout.rows ?? 24,
   halfWindow: 0,
   screenWidth: process.stdout.columns ?? 80,
   halfScreenWidth: 0,
+  chopLongLines: true,
   indentation: 2,
 };
