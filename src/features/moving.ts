0e8ecc86eeecc310399eda3ee61228fb937f6d69
Dawson Huang <dawsonhuang0@gmail.com>
2025-07-12 14:55:00 +1000

    perf: optimize lineForward and lineBackward traversal logic

--- src/features/moving.ts
+++ src/features/moving.ts
@@ -1,52 +1,74 @@
-import { maxSubRow, ringBell, offsetToNum } from "../helpers";
+import { maxSubRow, ringBell } from "../helpers";
 
 import { config, mode } from "../pagerConfig";
 
-export function lineForward(content: string[], offset: string): void {
+/**
+ * Forwards lines.
+ * 
+ * @param content string content array.
+ * @param offset lines to move forward.
+ */
+export function lineForward(content: string[], offset: number): void {
   if (mode.EOF) {
     ringBell();
     return;
   }
 
-  const n = offsetToNum(offset);
-
-  for (let i = 0; i < n; i++) {
-    if (config.subRow < maxSubRow(content[config.row])) {
-      config.subRow++;
-    } else {
-      config.row++;
-      config.subRow = 0;
-    }
-
-    if (config.row >= content.length) {
-      config.row = content.length - 1;
-
-      if (!config.chopLongLines) {
-        const maxSubRows = maxSubRow(content[config.row]);
-        if (config.subRow > maxSubRows) config.subRow = maxSubRows;
-      }
-
-      return;
-    }
+  if (config.chopLongLines) {
+    config.row = Math.min(config.row + offset, content.length - 1);
+    return;
   }
+
+  const remaining = maxSubRow(content[config.row]) - config.subRow;
+
+  if (remaining >= offset) {
+    config.subRow += offset;
+    return;
+  }
+
+  offset -= remaining + 1;
+  config.subRow = 0;
+  config.row++;
+
+  while (offset && config.row < content.length) {
+    config.subRow = Math.min(maxSubRow(content[config.row]), offset);
+    offset -= config.subRow + 1;
+    config.row++;
+  }
+
+  if (config.row === content.length) config.row = content.length - 1;
 }
 
-export function lineBackward(content: string[], offset: string): void {
+/**
+ * Backwards lines.
+ * 
+ * @param content string content array.
+ * @param offset lines to move backward.
+ */
+export function lineBackward(content: string[], offset: number): void {
   if (!config.row && !config.subRow) {
     ringBell();
     return;
   }
 
-  const n = offsetToNum(offset);
-
-  for (let i = 0; i < n; i++) {
-    if (!config.chopLongLines && config.subRow > 0) {
-      config.subRow--;
-    } else {
-      config.row--;
-      config.subRow = maxSubRow(content[config.row]);
-    }
-
-    if (!config.row && !config.subRow) return;
+  if (config.chopLongLines) {
+    config.row = Math.max(config.row - offset, 0);
+    return;
   }
+
+  if (config.subRow >= offset) {
+    config.subRow -= offset;
+    return;
+  }
+
+  offset -= config.subRow + 1;
+  config.row--;
+
+  while (offset && config.row >= 0) {
+    config.subRow = Math.max(maxSubRow(content[config.row]) - offset, 0);
+    offset -= config.subRow + 1;
+    config.row--;
+  }
+
+  if (config.row < 0) config.row = 0;
 }
