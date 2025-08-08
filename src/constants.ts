5a2be790abf5298729645c139eb648da170f994e
Dawson Huang <dawsonhuang0@gmail.com>
2025-08-08 16:56:37 +1000

    feat: add bold and underline ANSI constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,10 @@
 export const INVERSE_ON = '\x1b[7m';
 export const INVERSE_OFF = '\x1b[0m';
 
+export const BOLD_ON = '\x1b[1m';
+export const BOLD_OFF = '\x1b[0m';
+
+export const UNDERLINE_ON = '\x1B[4m';
+export const UNDERLINE_OFF = '\x1B[24m';
+
 export const END_MARKER = INVERSE_ON + '(END)' + INVERSE_OFF;
