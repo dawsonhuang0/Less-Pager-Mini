f3fc443c5e85811e952064c0487767f428368940
Dawson Huang <dawsonhuang0@gmail.com>
2025-08-09 01:39:00 +1000

    feat: add setter functions for config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { Config, Mode } from "./interfaces";
 /**
  * Global configuration for pager rendering and navigation.
  */
-export const config: Config = {
+export let config: Config = {
   row: 0,
   col: 0,
   subRow: 0,
@@ -20,8 +20,34 @@ export const config: Config = {
 /**
  * Tracks the current pager state.
  */
-export const mode: Record<Mode, boolean> = {
+export let mode: Record<Mode, boolean> = {
   'INIT': true,
   'EOF': false,
   'BUFFERING': false,
+  'HELP': false,
 }
+
+export const applyConfig = (newConfig: Config) => config = newConfig;
+
+export const applyMode = (newMode: Record<Mode, boolean>) => mode = newMode;
+
+export const resetConfig = () => config = {
+  row: 0,
+  col: 0,
+  subRow: 0,
+  setWindow: 0,
+  setHalfWindow: 0,
+  window: process.stdout.rows ?? 24,
+  halfWindow: (process.stdout.rows ?? 24) / 2,
+  screenWidth: process.stdout.columns ?? 80,
+  halfScreenWidth: (process.stdout.columns ?? 80) / 2,
+  chopLongLines: false,
+  indentation: 2,
+};
+
+export const resetMode = () => mode = {
+  'INIT': true,
+  'EOF': false,
+  'BUFFERING': false,
+  'HELP': false,
+};
