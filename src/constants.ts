a0dd3c778b182574a794c5ff86ef2146ee6f37c7
Dawson Huang <dawsonhuang0@gmail.com>
2025-12-22 05:03:38 +0800

    fix: use exact ansi disable code instead of reset

--- src/constants.ts
+++ src/constants.ts
@@ -11,13 +11,16 @@ export const CONSOLE_TITLE_RESET = CONSOLE_TITLE_START + CONSOLE_TITLE_END;
 export const ALTERNATE_CONSOLE_ON = '\x1b[?1049h';
 export const ALTERNATE_CONSOLE_OFF = '\x1b[?1049l';
 
+export const STYLE_RESET = '\x1b[0m';
+
 export const INVERSE_ON = '\x1b[7m';
-export const INVERSE_OFF = '\x1b[0m';
+export const INVERSE_OFF = '\x1b[27m';
 
 export const BOLD_ON = '\x1b[1m';
-export const BOLD_OFF = '\x1b[0m';
+export const BOLD_OFF = '\x1b[22m';
 
 export const UNDERLINE_ON = '\x1B[4m';
 export const UNDERLINE_OFF = '\x1B[24m';
 
+export const TILDE = BOLD_ON + '~' + BOLD_OFF;
 export const END_MARKER = INVERSE_ON + '(END)' + INVERSE_OFF;
